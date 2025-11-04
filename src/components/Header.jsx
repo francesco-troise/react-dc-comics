9b086c43c92972ff9b9a1c19eb16e4d0b3d74a9e
FrancescoTroise <francesco.troise98@gmail.com>
2025-11-04 18:11:49 +0100

    Create the "Header" component Insert the "Header" component into App.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,48 @@
+import logo from "../assets/img/dc-logo.png";
 
-export default function Header(){
+export default function Header() {
+  return (
+    <header className="bg-white">
+      <nav className="navbar py-2">
+        <div className="container">
+          <a className="navbar-brand " href="#">
+            <img src={logo} alt="logo" className="logo-navbar" />
+          </a>
 
-    return(
-        
-    )
+          <div className="navbar-nav flex-row gap-4 fw-semibold">
+            <a className="nav-link " href="#">
+              Characters
+            </a>
+            <a className="nav-link" href="#">
+              Comics
+            </a>
+            <a className="nav-link" href="#">
+              Movies
+            </a>
+            <a className="nav-link " href="#">
+              Tv
+            </a>
+            <a className="nav-link " href="#">
+              Games
+            </a>
+            <a className="nav-link" href="#">
+              Collectibles
+            </a>
+            <a className="nav-link" href="#">
+              Video
+            </a>
+            <a className="nav-link " href="#">
+              Fans
+            </a>
+            <a className="nav-link" href="#">
+              News
+            </a>
+            <a className="nav-link " href="#">
+              Shop
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
